cddf4cc6c0210fb3588c16035d1a5cf9efe356d9
Ankit Kumar Jha <140503877+ankit442660@users.noreply.github.com>
2024-08-31 17:26:20 +0530

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -5,7 +5,7 @@ const cors=require("cors");
 
 const express=require('express');
 const connectDb=require("./utils/db")
-const port=process.env.PORT||5000;
+const port=process.env.PORT;
 const authRoute=require("./Router/auth-router");
 const errormiddleware = require('./middlewares/error-middleware');
 const app=express();
